fc8ec804c0475b22f1445458d9a01c1157888d70
syh <1147948702@qq.com>
2021-10-13 22:36:18 +0800

    添加项目文件。

---
+++ WebApplication1/Models/Acc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class Acc
+	{
+		public int Test { get; set; }
+	}
+}
