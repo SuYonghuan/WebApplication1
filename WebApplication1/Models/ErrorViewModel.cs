70d0fb75fba8c29c72f1dd518f2177c42d5f2baf
syh <1147948702@qq.com>
2021-10-13 23:43:31 +0800

    11

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -2,10 +2,12 @@ using System;
 
 namespace WebApplication1.Models
 {
-	public class ErrorViewModel
-	{
-		public string RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public int number { set; get; }
+    }
 }
